a77262591522d00591913d833f8f5612cf3a46a3
Mary Gwozdz <mgwozdz@unicon.net>
2021-09-27 07:50:49 -0700

    L3-5 Moved response from POST for config endpoint from headers to body, added unit tests, and made code quality improvements (lombok, slf4j, etc)

--- src/main/java/net/unicon/lti/service/lti/impl/LTIJWTServiceImpl.java
+++ src/main/java/net/unicon/lti/service/lti/impl/LTIJWTServiceImpl.java
@@ -141,10 +141,10 @@ public class LTIJWTServiceImpl implements LTIJWTService {
         Key toolPrivateKey = OAuthUtils.loadPrivateKey(ltiDataService.getOwnPrivateKey());
         String aud;
         //D2L needs a different aud, maybe others too
-        if (platformDeployment.getoAuth2TokenAud() != null) {
-            aud = platformDeployment.getoAuth2TokenAud();
+        if (platformDeployment.getOAuth2TokenAud() != null) {
+            aud = platformDeployment.getOAuth2TokenAud();
         } else {
-            aud = platformDeployment.getoAuth2TokenUrl();
+            aud = platformDeployment.getOAuth2TokenUrl();
         }
         String state = Jwts.builder()
                 .setHeaderParam("kid", TextConstants.DEFAULT_KID)
