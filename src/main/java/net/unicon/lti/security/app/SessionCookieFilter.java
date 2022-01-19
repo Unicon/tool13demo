7559ab0fa163887fe6d010e406fa77ffd5d0654d
Mary Gwozdz <mgwozdz@unicon.net>
2022-01-19 11:49:20 -0700

    Revert "Fully remove unneeded SessionCookieFilter, turn demo mode back off for oidc controller, remove unneeded manual lti cookies"
    This reverts commit adf177f29aacccc2661da4e1f433b9cd4bf625e1.

---
+++ src/main/java/net/unicon/lti/security/app/SessionCookieFilter.java
@@ -0,0 +1,66 @@
+package net.unicon.lti.security.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+public class SessionCookieFilter extends GenericFilterBean {
+
+    private final List<String> PATHS_TO_IGNORE_SETTING_SAMESITE = List.of("resources");
+    private final String SESSION_COOKIE_NAME = "JSESSIONID";
+    private final String SESSION_PATH_ATTRIBUTE = ";Path=";
+    private final String ROOT_CONTEXT = "/";
+    private final String SAME_SITE_ATTRIBUTE_VALUES = ";HttpOnly;Secure;SameSite=None";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("inside post oidc filter");
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String requestUrl = req.getRequestURL().toString();
+        boolean isResourceRequest = requestUrl != null ? StringUtils.isNoneBlank(PATHS_TO_IGNORE_SETTING_SAMESITE.stream().filter(s -> requestUrl.contains(s)).findFirst().orElse(null)) : null;
+
+        log.info("Request Session Id in SessionCookieFilter: {}", ((HttpServletRequest) request).getSession().getId());
+        log.info("Request URL in SessionCookieFilter: {}", ((HttpServletRequest) request).getRequestURL().toString());
+        log.info("Request URI in SessionCookieFilter: {}", ((HttpServletRequest) request).getRequestURI());
+        log.info("Request Method in SessionCookieFilter: {}", ((HttpServletRequest) request).getMethod());
+        log.info("Request Set-Cookie Header in SessionCookieFilter: {}", ((HttpServletRequest) request).getHeader("set-cookie"));
+        log.info("Response Set-Cookie Header in SessionCookieFilter: {}", ((HttpServletResponse) response).getHeader("set-cookie"));
+        log.info("Request Cookies in SessionCookieFilter: {}", ((HttpServletRequest) request).getCookies() != null ? Arrays.stream(((HttpServletRequest) request).getCookies()).toList().toString() : null);
+        Cookie[] loggingCookies = ((HttpServletRequest) request).getCookies();
+        if (loggingCookies != null) {
+            for (Cookie cookie : loggingCookies) {
+                log.info("Cookie name: {}", cookie.getName());
+                log.info("Cookie value: {}", cookie.getValue());
+            }
+        }
+
+        if (!isResourceRequest) {
+            log.info("is not resource request");
+            Cookie[] cookies = ((HttpServletRequest) request).getCookies();
+            if (cookies != null && cookies.length > 0) {
+                log.info("{} cookies present in post oidc filter", cookies.length);
+                List<Cookie> cookieList = Arrays.asList(cookies);
+                Cookie sessionCookie = cookieList.stream().filter(cookie -> SESSION_COOKIE_NAME.equals(cookie.getName())).findFirst().orElse(null);
+                if (sessionCookie != null) {
+                    String contextPath = request.getServletContext() != null && StringUtils.isNotBlank(request.getServletContext().getContextPath()) ? request.getServletContext().getContextPath() : ROOT_CONTEXT;
+                    resp.setHeader(HttpHeaders.SET_COOKIE, sessionCookie.getName() + "=" + sessionCookie.getValue() + SESSION_PATH_ATTRIBUTE + contextPath + SAME_SITE_ATTRIBUTE_VALUES);
+                }
+            }
+        }
+        chain.doFilter(request, response);
+    }
+}
