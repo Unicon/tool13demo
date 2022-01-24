708ef5e694c759f8cf1e0fb390f093ca2dde9d3e
Mary Gwozdz <mgwozdz@unicon.net>
2022-01-24 07:50:07 -0700

    Use javascript for external post redirect

--- src/main/java/net/unicon/lti/controller/lti/LTI3Controller.java
+++ src/main/java/net/unicon/lti/controller/lti/LTI3Controller.java
@@ -12,7 +12,6 @@
  */
 package net.unicon.lti.controller.lti;
 
-import com.google.common.io.ByteStreams;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.SignatureException;
@@ -26,16 +25,11 @@ import net.unicon.lti.utils.TextConstants;
 import net.unicon.lti.utils.lti.LTI3Request;
 import net.unicon.lti.utils.lti.LtiOidcUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -43,12 +37,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
@@ -74,7 +65,7 @@ public class LTI3Controller {
     private CloseableHttpClient client = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
 
     @PostMapping(value={"/lti3","/lti3/"}, produces = MediaType.TEXT_HTML_VALUE)
-    public void lti3(HttpServletRequest req, HttpServletResponse res)  {
+    public String lti3(HttpServletRequest req, HttpServletResponse res, Model model)  {
         //First we will get the state, validate it
         String state = req.getParameter("state");
         //We will use this link to find the content to display.
@@ -100,41 +91,46 @@ public class LTI3Controller {
                 String target = lti3Request.getLtiTargetLinkUrl();
                 log.debug(target);
                 String ltiData = LtiOidcUtils.generateLtiToken(lti3Request, ltiDataService);
-                HttpEntity entity = MultipartEntityBuilder.create().addTextBody("id_token", ltiData).build();
-                String redirect = UriComponentsBuilder.fromUriString(target).build().toUriString();
-                HttpPost httpPost = new HttpPost(redirect);
-                httpPost.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ltiDataService.getLocalUrl());
-                httpPost.setEntity(entity);
-//                URI redirect = UriComponentsBuilder.fromUriString(target).queryParam("id_token", ltiData).build().toUri();
-//                HttpPost httpPost = new HttpPost(redirect);
-                CloseableHttpResponse response = client.execute(httpPost);
-//                return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT).location(UriComponentsBuilder.fromUriString(target).queryParam("id_token", ltiData).build().toUri()).build();
 
-                if (response.getStatusLine().getStatusCode() != HttpStatus.OK.value()) {
-                    log.error("Unsuccessful Post to {}", redirect.toString());
-                    log.error(String.valueOf(response.getStatusLine().getStatusCode()));
-                    log.error(response.getStatusLine().getReasonPhrase());
-                    log.error(new String(response.getEntity().getContent().readAllBytes()));
-//                    return "Unsuccessful post to application: " + response.getStatusLine().toString();
-                    ByteStreams.copy(new ByteArrayInputStream(("Unsuccessful post to application: " + response.getStatusLine().toString()).getBytes()), res.getOutputStream());
-                } else {
-                    res.setContentType(MediaType.TEXT_HTML_VALUE);
-//                    return "post to application returned Http Status OK";
-                    ByteStreams.copy(response.getEntity().getContent(), res.getOutputStream());
-                }
+                model.addAttribute("target", target);
+                model.addAttribute("id_token", ltiData);
+                return "lti3Redirect";
+
+//                HttpEntity entity = MultipartEntityBuilder.create().addTextBody("id_token", ltiData).build();
+//                String redirect = UriComponentsBuilder.fromUriString(target).build().toUriString();
+//                HttpPost httpPost = new HttpPost(redirect);
+//                httpPost.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ltiDataService.getLocalUrl());
+//                httpPost.setEntity(entity);
+////                URI redirect = UriComponentsBuilder.fromUriString(target).queryParam("id_token", ltiData).build().toUri();
+////                HttpPost httpPost = new HttpPost(redirect);
+//                CloseableHttpResponse response = client.execute(httpPost);
+////                return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT).location(UriComponentsBuilder.fromUriString(target).queryParam("id_token", ltiData).build().toUri()).build();
+//
+//                if (response.getStatusLine().getStatusCode() != HttpStatus.OK.value()) {
+//                    log.error("Unsuccessful Post to {}", redirect.toString());
+//                    log.error(String.valueOf(response.getStatusLine().getStatusCode()));
+//                    log.error(response.getStatusLine().getReasonPhrase());
+//                    log.error(new String(response.getEntity().getContent().readAllBytes()));
+////                    return "Unsuccessful post to application: " + response.getStatusLine().toString();
+//                    ByteStreams.copy(new ByteArrayInputStream(("Unsuccessful post to application: " + response.getStatusLine().toString()).getBytes()), res.getOutputStream());
+//                } else {
+////                    res.setContentType(MediaType.TEXT_HTML_VALUE);
+////                    return "post to application returned Http Status OK";
+////                    ByteStreams.copy(response.getEntity().getContent(), res.getOutputStream());
+//                }
             }
             else {
-//                return "demo mode enabled";
+                return "demo mode enabled";
 //                return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(ltiDataService.getLocalUrl() + "/demo?link=" + link)).build();
-                res.sendRedirect("/demo?link=" + link);
+//                res.sendRedirect("/demo?link=" + link);
             }
         } catch (SignatureException ex) {
 //            return "invalid signature";
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid signature");
-        } catch (IOException ex) {
-//            return "io exception";
-            ex.printStackTrace();
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad Request");
+//        } catch (IOException ex) {
+////            return "io exception";
+//            ex.printStackTrace();
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad Request");
         } catch (GeneralSecurityException ex) {
 //            return "general security exception";
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error");
