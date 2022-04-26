e4501703fc63f355c2ff3d6d41649d06b1e92189
Mary Gwozdz <mgwozdz@unicon.net>
2022-04-26 07:21:30 -0700

    Fix for Deep Linking in D2L Brightspace

--- src/main/java/net/unicon/lti/controller/lti/JWKController.java
+++ src/main/java/net/unicon/lti/controller/lti/JWKController.java
@@ -13,11 +13,10 @@
 package net.unicon.lti.controller.lti;
 
 
+import lombok.extern.slf4j.Slf4j;
 import net.unicon.lti.service.lti.LTIDataService;
 import net.unicon.lti.utils.TextConstants;
 import net.unicon.lti.utils.oauth.OAuthUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -30,6 +29,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.security.GeneralSecurityException;
 import java.security.interfaces.RSAPublicKey;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
@@ -38,27 +38,36 @@ import java.util.Map;
 /**
  * Serving the public key of the tool.
  */
+@Slf4j
 @Controller
 @Scope("session")
 @RequestMapping("/jwks")
 public class JWKController {
-
     @Autowired
     LTIDataService ltiDataService;
 
-    static final Logger log = LoggerFactory.getLogger(JWKController.class);
-
     @RequestMapping(value = "/jwk", method = RequestMethod.GET, produces = "application/json;")
     @ResponseBody
-    public Map<String, List<Map<String, Object>>> jkw(HttpServletRequest req, Model model) throws GeneralSecurityException {
+    public Map<String, List<Map<String, Object>>> jwk(HttpServletRequest req, Model model) throws GeneralSecurityException {
         Map<String, List<Map<String, Object>>> keys = new HashMap<>();
         log.debug("Someone is calling the jwk endpoint!");
         log.debug(req.getQueryString());
         RSAPublicKey toolPublicKey = OAuthUtils.loadPublicKey(ltiDataService.getOwnPublicKey());
+
+        // There is a .NET bug that affects D2L Brightspace making it such that it is not capable of reading
+        // the prefix byte (the 00 byte indicating it is a positive integer) that gets added to the modulus (n), so
+        // this byte must be removed for D2L to be able to use the JWKS endpoint.
+        // Sources:
+        // https://community.brightspace.com/s/article/LTI-Advantage-Developer-FAQ-s
+        // https://datatracker.ietf.org/doc/html/rfc7518#section-6.3.1.1
+        byte[] modulus = toolPublicKey.getModulus().toByteArray();
+        log.debug("Modulus Length: " + modulus.length);
+        byte[] modulusWithoutPrefix = Arrays.copyOfRange(modulus, 1, modulus.length);
+
         Map<String, Object> values = new HashMap<>();
         values.put("kty", toolPublicKey.getAlgorithm()); // getAlgorithm() returns kty not algorithm
         values.put("kid", TextConstants.DEFAULT_KID);
-        values.put("n", Base64.getUrlEncoder().encodeToString(toolPublicKey.getModulus().toByteArray()));
+        values.put("n", Base64.getUrlEncoder().encodeToString(modulusWithoutPrefix));
         values.put("e", Base64.getUrlEncoder().encodeToString(toolPublicKey.getPublicExponent().toByteArray()));
         values.put("alg", "RS256");
         values.put("use", "sig");
