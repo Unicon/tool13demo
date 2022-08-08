681703589e94ee6955cfe59f1081f6d714c29d31
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2022-08-08 14:27:13 +0200

    DL3-53 Provide consistency according to the FIGMA designs.

--- src/main/frontend/src/features/CourseCard.js
+++ src/main/frontend/src/features/CourseCard.js
@@ -13,10 +13,10 @@ function CourseCard (props) {
 
   return (
     <Card onClick={(e) => dispatch(changeSelectedCourse(props.course))} className="course-card">
-      <Card.Img variant="top" src={courseCoverUrl} className="course-image" title={props.course.book_title}/>
+      <Card.Img variant="top" src={courseCoverUrl} className="course-card-image " title={props.course.book_title}/>
       <Card.Body>
-        <Card.Title className="mb-2">{props.course.book_title ? props.course.book_title : 'This course does not have a title.'}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Released: {props.course.release_date ? formatDate(props.course.release_date) : 'No release date has been provided.'}</Card.Subtitle>
+        <Card.Title className="course-card-title">{props.course.book_title ? props.course.book_title : 'This course does not have a title.'}</Card.Title>
+        <Card.Subtitle className="course-card-subtitle">Released: {props.course.release_date ? formatDate(props.course.release_date) : 'No release date has been provided.'}</Card.Subtitle>
       </Card.Body>
     </Card>
   );
