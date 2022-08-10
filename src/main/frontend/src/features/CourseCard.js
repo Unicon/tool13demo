7970167f68a0ad24872917a40a168f75591a130d
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2022-08-10 07:37:32 +0200

    DL3-54 Accessibility Issue on Course Select UI - Can not select a Course using only keyboard (#56)

--- src/main/frontend/src/features/CourseCard.js
+++ src/main/frontend/src/features/CourseCard.js
@@ -11,13 +11,23 @@ function CourseCard (props) {
   // Some courses may not have a valid cover image, use a default instead
   const courseCoverUrl = parseCourseCoverImage(props.course.cover_img_url);
 
+  // If the pressed key is the enter we have to select the course.
+  const checkPressedKey = (e) => {
+    if (e.key === 'Enter') {
+      dispatch(changeSelectedCourse(props.course));
+    }
+  }
+
   return (
-    <Card onClick={(e) => dispatch(changeSelectedCourse(props.course))} className="course-card">
-      <Card.Img variant="top" src={courseCoverUrl} className="course-card-image " title={props.course.book_title}/>
-      <Card.Body>
-        <Card.Title className="course-card-title">{props.course.book_title ? props.course.book_title : 'This course does not have a title.'}</Card.Title>
-        <Card.Subtitle className="course-card-subtitle">Released: {props.course.release_date ? formatDate(props.course.release_date) : 'No release date has been provided.'}</Card.Subtitle>
-      </Card.Body>
+    <Card className="course-card"
+      tabindex="0"
+      onKeyPress={(e) => checkPressedKey(e)}
+      onClick={(e) => dispatch(changeSelectedCourse(props.course))} >
+        <Card.Img variant="top" src={courseCoverUrl} className="course-card-image" title={props.course.book_title}/>
+        <Card.Body>
+          <Card.Title className="course-card-title">{props.course.book_title ? props.course.book_title : 'This course does not have a title.'}</Card.Title>
+          <Card.Subtitle className="course-card-subtitle">Released: {props.course.release_date ? formatDate(props.course.release_date) : 'No release date has been provided.'}</Card.Subtitle>
+        </Card.Body>
     </Card>
   );
 
