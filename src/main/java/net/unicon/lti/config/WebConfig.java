8c8f4d28d6cab6d15a10dfbcfd420c4907b5ede1
Mary Gwozdz <mgwozdz@unicon.net>
2022-09-06 11:42:01 -0700

    DL3-83 comment out test code

--- src/main/java/net/unicon/lti/config/WebConfig.java
+++ src/main/java/net/unicon/lti/config/WebConfig.java
@@ -46,8 +46,15 @@ public class WebConfig implements WebMvcConfigurer {
                         "classpath:/static/static/css/"
                         );
 
+//        Resource resource = resourceLoader.getResource("classpath:/static/index.html");
+//        if (resource.exists()) {
+//            System.out.println("Resource exists");
+//        } else {
+//            System.out.println("resource does not exist");
+//        }
+
         registry.addResourceHandler("/index.html")
-                .addResourceLocations("classpath:/frontend/public/index.html")
+                .addResourceLocations("classpath:/static/index.html")
                 .setCachePeriod(0);
     }
 
